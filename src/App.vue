7c2ece66963b053554b49724e46308c9ce721ee8
Sergii <74363342+akoross@users.noreply.github.com>
2023-04-05 18:38:30 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Cron from './components/Cron.vue'
+</script>
+
+<template>
+  <Cron />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Cron.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import {onMounted, ref, watch} from 'vue'
+import cronstrue from 'cronstrue'
+
+const inputCron = ref('* * * * * *')
+const cronText = ref('')
+
+onMounted(() => {
+  cronText.value = cronstrue.toString(inputCron.value)
+})
+
+const printCron = () => {
+  try {
+    cronText.value = cronstrue.toString(inputCron.value)
+  } catch (error) {
+    cronText.value = error
+  }
+}
+watch(inputCron, async () => {
+  try {
+    cronText.value = cronstrue.toString(inputCron.value)
+  } catch (error) {
+    cronText.value = error
+  }
+})
+</script>
+
+<template>
+    <input v-model="inputCron" @change="printCron" class="input" type="text">
+    <p class="cron-info">{{ cronText }}</p>
+</template>
+
+<style lang="css" scoped>
+.input {
+  font-size: 1.5rem;
+  text-align: center;
+}
+.cron-info {
+  max-width: 320px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  font-size: 1.3rem;
+  line-height: 1.7rem;
+}
+</style>
